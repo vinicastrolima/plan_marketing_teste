bfd4df13b1c0d10242c6fd5d26de055989d9558c
Vinícius Castro <vinicastrolimavcl@gmail.com>
2023-08-16 02:25:46 -0300

    Feature => Alteração da Home, Inclusão do VueX e VueRouter

---
+++ frontend/src/store/index.js
@@ -0,0 +1,18 @@
+import { createStore } from 'vuex';
+
+const store = createStore({
+  state: {
+    // Defina seu estado inicial aqui
+  },
+  mutations: {
+    // Defina suas mutações aqui
+  },
+  actions: {
+    // Defina suas ações aqui
+  },
+  getters: {
+    // Defina seus getters aqui
+  },
+});
+
+export default store;
---
+++ frontend/src/views/HomePage.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="home-page">
+    <div class="container mt-5">
+      <div class="card">
+        <div class="card-header">
+          Lista de Itens
+          <button class="btn btn-primary float-end" @click="addItem">Adicionar Item</button>
+        </div>
+        <div class="card-body">
+          <table class="table table-bordered">
+            <thead class="table-dark">
+              <tr>
+                <th scope="col">Nome</th>
+                <th scope="col">Descrição</th>
+                <th scope="col">Tensão</th>
+                <th scope="col">Marca</th>
+                <th scope="col">Ações</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="item in items" :key="item.id">
+                <td>{{ item.nome }}</td>
+                <td>{{ item.descricao }}</td>
+                <td>{{ item.tensao }}</td>
+                <td>{{ item.marca }}</td>
+                <td>
+                  <button class="btn btn-primary me-2" @click="editItem(item.id)">Editar</button>
+                  <button class="btn btn-danger" @click="deleteItem(item.id)">Excluir</button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      items: [],
+    };
+  },
+  methods: {
+    async fetchItems() {
+      try {
+        const response = await axios.get('http://127.0.0.1:8000/api/eletrodomesticos');
+        this.items = response.data;
+      } catch (error) {
+        console.error('Erro ao buscar itens:', error);
+      }
+    },
+    addItem() {
+      // Implemente a lógica para adicionar um novo item
+    },
+    editItem() {
+      // Implemente a lógica para editar um item
+    },
+    deleteItem() {
+      // Implemente a lógica para excluir um item
+    },
+  },
+  mounted() {
+    this.fetchItems();
+  },
+};
+</script>
+
+<style>
+/* Adicione estilos CSS aqui, se necessário */
+</style>
